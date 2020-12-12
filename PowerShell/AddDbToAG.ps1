b34dddfc9c494249fb0c04945ff3c36f00a20037
relsna <steven.naudet@gmail.com>
2020-12-12 21:00:11 +0100

    Listener parameter

--- PowerShell/AddDbToAG.ps1
+++ PowerShell/AddDbToAG.ps1
@@ -4,35 +4,29 @@ Take a Primary Replica instance as input
     - Add all databases to the Availability Group.
     - Maunal Seeding => Backup -> Copy -> Restore
 
-
 #inspired by code from ijeb
 #https://github.com/sqlcollaborative/dbatools/issues/4610
 
-
 #  todo:  restore login SA
-#  todo:  prendre en compte le cas o˘ la base n'est pas dÈj‡ dans l'AG
 #>
 
+$listener = 'LSTN-APP-QUAL'
+
+$primaryReplica =    Get-DbaAgReplica -SqlInstance $listener | Where-Object Role -eq Primary
+$secondaryReplicas = Get-DbaAgReplica -SqlInstance $listener | Where-Object Role -eq Secondary
+$AG = (Get-DbaAvailabilityGroup -SqlInstance $primaryReplica.Name).AvailabilityGroup
+$databases = Get-DbaDatabase -SqlInstance $primaryReplica.Name
+
 $allbackups = @{}
-$Instance = 'primaryInstance' #primary instance
-$AG = 'AvailabilityGroupName'
 
-#$Database = Get-DbaAgDatabase -SqlInstance $Instance -AvailabilityGroup $AG
-$Database = Get-DbaDatabase -SqlInstance $Instance
-$primary = Get-DbaAgReplica -SqlInstance $Instance | Where-Object Role -eq 'Primary'
-$secondaries = Get-DbaAgReplica -SqlInstance $Instance | Where-Object Role -eq 'Secondary'
-
-foreach ($db in $Database) {
-    #Write-Output $db
-    $primaryDb = Get-DbaDatabase -SqlInstance $Primary.Name -Database $db.Name
+foreach ($db in $databases) {
+    $primaryDb = Get-DbaDatabase -SqlInstance $primaryReplica.Name -Database $db.Name
     
-    foreach ($second in $secondaries) {
-        #Write-Output $second
-        #$primaryDb = Get-DbaDatabase -SqlInstance $Primary.Name -Database $db.Name
+    foreach ($second in $secondaryReplicas) {
         Write-Output "priamry: " +$db
         $secondaryDb = Get-DbaDatabase -SqlInstance $second.Name -Database $db.Name
-        #Write-Output $secondaryDb
         Write-Output "secondary: " +$secondaryDb
+
         if (-not $secondaryDb) {
             Write-Output $db.Name
             # Backup Primary 
@@ -41,14 +35,12 @@ foreach ($db in $Database) {
             $allbackups[$db] = $fullbackup, $logbackup
 
             foreach ($file in $allbackups[$db]) {
-                #Write-Output $file.Path
                 $backupPath =  $file.Path -replace ':', '$'
-                $primaryHost = $Primary.Name.Substring(0, $Primary.Name.IndexOf('\')+1)
+                $primaryHost = $primaryReplica.Name.Substring(0, $primaryReplica.Name.IndexOf('\')+1)
                 $secondaryHost = $second.Name.Substring(0, $second.Name.IndexOf('\')+1)
 
                 $secondaryPath = '\\' + $secondaryHost + $backupPath
                 $primaryPath = '\\' + $primaryHost + $backupPath
-                #Write-Output $secondaryPath
                 Write-Output $primaryPath
 
                 Copy-Item -Path $primaryPath -Destination $secondaryPath
@@ -56,12 +48,12 @@ foreach ($db in $Database) {
         
             $allbackups[$db] | Restore-DbaDatabase -SqlInstance $second.Name -WithReplace -NoRecovery -EnableException
         
-            #Check si la DB est dÈj‡ dans le groupe ou pas
-            $agInDb = Get-DbaAgDatabase -SqlInstance $Instance -AvailabilityGroup $AG | Where-Object Name -eq $db.Name
+            #Check si la DB est d√©j√† dans le groupe ou pas
+            $agInDb = Get-DbaAgDatabase -SqlInstance $primaryReplica.Name -AvailabilityGroup $AG | Where-Object Name -eq $db.Name
             if (-not $agInDb) {
                 Write-Output "not agInDb"
                 $query = "ALTER AVAILABILITY GROUP [$($AG)] ADD DATABASE [$($db.Name)]" 
-                Invoke-DbaQuery -SqlInstance $Instance -Query $query
+                Invoke-DbaQuery -SqlInstance $primaryReplica.Name -Query $query
             }
             Write-Output "set hadr"
             #Add-DbaAgDatabase -SqlInstance $second.Name -AvailabilityGroup $AG -Database $db.Name -Secondary
@@ -72,5 +64,4 @@ foreach ($db in $Database) {
 
     } # foreach ($second in $secondaries)
 }
-
 
