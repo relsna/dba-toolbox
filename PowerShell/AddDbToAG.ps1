7518ad6b41eab2bd198df4e1bd3774f14f353cb4
relsna <steven.naudet@gmail.com>
2020-12-16 17:47:40 +0100

    function + multiple ag/lsn

--- PowerShell/AddDbToAG.ps1
+++ PowerShell/AddDbToAG.ps1
@@ -10,58 +10,69 @@ Take a Primary Replica instance as input
 #  todo:  restore login SA
 #>
 
-$listener = 'LSTN-APP-QUAL'
+# Add-DbiAgDatabase -Listener 'LST-APP-QUAL' -Verbose
 
-$primaryReplica =    Get-DbaAgReplica -SqlInstance $listener | Where-Object Role -eq Primary
-$secondaryReplicas = Get-DbaAgReplica -SqlInstance $listener | Where-Object Role -eq Secondary
-$AG = (Get-DbaAvailabilityGroup -SqlInstance $primaryReplica.Name).AvailabilityGroup
-$databases = Get-DbaDatabase -SqlInstance $primaryReplica.Name
-
-$allbackups = @{}
-
-foreach ($db in $databases) {
-    $primaryDb = Get-DbaDatabase -SqlInstance $primaryReplica.Name -Database $db.Name
+function Add-DbiAgDatabase {
     
-    foreach ($second in $secondaryReplicas) {
-        Write-Output "priamry: " +$db
-        $secondaryDb = Get-DbaDatabase -SqlInstance $second.Name -Database $db.Name
-        Write-Output "secondary: " +$secondaryDb
+    param (
+        [Parameter(Mandatory=$true)]$listener
+    )
 
-        if (-not $secondaryDb) {
-            Write-Output $db.Name
-            # Backup Primary 
-            $fullbackup = $primarydb | Backup-DbaDatabase -Checksum -CompressBackup -Type Full -EnableException -Initialize
-            $logbackup = $primarydb | Backup-DbaDatabase -Checksum -CompressBackup -Type Log -EnableException -Initialize
-            $allbackups[$db] = $fullbackup, $logbackup
+    $primaryReplica =    Get-DbaAgReplica -SqlInstance $listener | Select-Object -Unique | Where-Object Role -eq Primary
+    $secondaryReplicas = Get-DbaAgReplica -SqlInstance $listener | Select-Object -Unique | Where-Object Role -eq Secondary
+    # Get only the AG related to the listener. Excluse other AG on the instance
+    $AG = (Get-DbaAgListener -SqlInstance $listener | Where-Object Name -eq $listener).AvailabilityGroup
 
-            foreach ($file in $allbackups[$db]) {
-                $backupPath =  $file.Path -replace ':', '$'
-                $primaryHost = $primaryReplica.Name.Substring(0, $primaryReplica.Name.IndexOf('\')+1)
-                $secondaryHost = $second.Name.Substring(0, $second.Name.IndexOf('\')+1)
+    $allbackups = @{}
+    $databases = Get-DbaDatabase -SqlInstance $primaryReplica.Name
 
-                $secondaryPath = '\\' + $secondaryHost + $backupPath
-                $primaryPath = '\\' + $primaryHost + $backupPath
-                Write-Output $primaryPath
-
-                Copy-Item -Path $primaryPath -Destination $secondaryPath
-            }
+    foreach ($db in $databases) {
+        $primaryDb = Get-DbaDatabase -SqlInstance $primaryReplica.Name -Database $db.Name
         
-            $allbackups[$db] | Restore-DbaDatabase -SqlInstance $second.Name -WithReplace -NoRecovery -EnableException
-        
-            #Check si la DB est déjà dans le groupe ou pas
-            $agInDb = Get-DbaAgDatabase -SqlInstance $primaryReplica.Name -AvailabilityGroup $AG | Where-Object Name -eq $db.Name
-            if (-not $agInDb) {
-                Write-Output "not agInDb"
-                $query = "ALTER AVAILABILITY GROUP [$($AG)] ADD DATABASE [$($db.Name)]" 
-                Invoke-DbaQuery -SqlInstance $primaryReplica.Name -Query $query
-            }
-            Write-Output "set hadr"
-            #Add-DbaAgDatabase -SqlInstance $second.Name -AvailabilityGroup $AG -Database $db.Name -Secondary
-            $query = "ALTER DATABASE [$($db.Name)] SET HADR AVAILABILITY GROUP = [$($AG)]"
-            Invoke-DbaQuery -SqlInstance $second.Name -Query $query
+        foreach ($second in $secondaryReplicas) {
+            Write-Verbose "priamry:  $db"
+            $secondaryDb = Get-DbaDatabase -SqlInstance $second.Name -Database $db.Name
+            Write-Verbose "secondary:  $secondaryDb"
 
-        } # if (-not $secondaryDb)
+            if (-not $secondaryDb) {
+                Write-Verbose $db.Name
+                # Backup Primary 
+                $fullbackup = $primarydb | Backup-DbaDatabase -Checksum -CompressBackup -Type Full -EnableException -Initialize
+                $logbackup = $primarydb | Backup-DbaDatabase -Checksum -CompressBackup -Type Log -EnableException -Initialize
+                $allbackups[$db] = $fullbackup, $logbackup
 
-    } # foreach ($second in $secondaries)
+                foreach ($file in $allbackups[$db]) {
+                    $backupPath =  $file.Path -replace ':', '$'
+                    $primaryHost = $primaryReplica.Name.Substring(0, $primaryReplica.Name.IndexOf('\')+1)
+                    $secondaryHost = $second.Name.Substring(0, $second.Name.IndexOf('\')+1)
+
+                    $secondaryPath = '\\' + $secondaryHost + $backupPath
+                    $primaryPath = '\\' + $primaryHost + $backupPath
+                    Write-Verbose $primaryPath
+
+                    Copy-Item -Path $primaryPath -Destination $secondaryPath
+                }
+            
+                $allbackups[$db] | Restore-DbaDatabase -SqlInstance $second.Name -WithReplace -NoRecovery -EnableException
+            
+                #Check si la DB est déjà dans le groupe ou pas
+                $agInDb = Get-DbaAgDatabase -SqlInstance $primaryReplica.Name -AvailabilityGroup $AG | Where-Object Name -eq $db.Name
+                if (-not $agInDb) {
+                    Write-Verbose "not agInDb"
+                    $query = "ALTER AVAILABILITY GROUP [$($AG)] ADD DATABASE [$($db.Name)]" 
+                    Invoke-DbaQuery -SqlInstance $primaryReplica.Name -Query $query
+                }
+                Write-Verbose "set hadr"
+                $agInDb = Get-DbaAgDatabase -SqlInstance $second.Name -AvailabilityGroup $AG | Where-Object Name -eq $db.Name
+                if (-not $agInDb) {
+                    Write-Verbose "set hadr2"
+                    #Add-DbaAgDatabase -SqlInstance $second.Name -AvailabilityGroup $AG -Database $db.Name -Secondary
+                    $query = "ALTER DATABASE [$($db.Name)] SET HADR AVAILABILITY GROUP = [$($AG)]"
+                    Invoke-DbaQuery -SqlInstance $second.Name -Query $query
+                }
+
+            } # if (-not $secondaryDb)
+
+        } # foreach ($second in $secondaries)
+    }
 }
-
